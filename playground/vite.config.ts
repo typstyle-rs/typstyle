0c863ce1860a9b8358658d2954fe61297c7bd3e6
QuadnucYard <quadnucyard@gmail.com>
2026-01-25 23:56:16 +0800

    build(web): update npm deps for playground

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -1,11 +1,12 @@
 /// <reference types="vitest/config" />
-import { defineConfig } from "vite";
-import wasm from "vite-plugin-wasm";
-import react from "@vitejs/plugin-react";
-import toplevelAwait from "vite-plugin-top-level-await";
-import tailwindcss from "@tailwindcss/vite";
+
 import * as path from "node:path";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 import { viteStaticCopy } from "vite-plugin-static-copy";
+import toplevelAwait from "vite-plugin-top-level-await";
+import wasm from "vite-plugin-wasm";
 
 // https://vite.dev/config/
 export default defineConfig({
